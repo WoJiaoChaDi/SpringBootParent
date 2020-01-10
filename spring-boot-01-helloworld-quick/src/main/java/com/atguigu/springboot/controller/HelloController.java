bae01822d5d6a0e128e5f0a028ec7338d485e061
wojiaochadi <264835229@qq.com>
2020-01-10 16:38:37 +0800

    用Spring Initializer 快去构建 SpringBoot

---
+++ spring-boot-01-helloworld-quick/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.atguigu.springboot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//这个类的所有方法返回的数据直接写给浏览器，（如果是对象转为json数据）
+/*@ResponseBody
+@Controller*/
+//RestController = @Controller + @ResponseBody
+@RestController
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world quick!";
+    }
+
+    // RESTAPI的方式
+}
