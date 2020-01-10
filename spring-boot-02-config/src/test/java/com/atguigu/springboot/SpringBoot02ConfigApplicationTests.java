969a04f84f88d88e52e660ef128ef5a393963cd9
wojiaochadi <264835229@qq.com>
2020-01-10 17:52:34 +0800

    @ImportResource：导入Spring的配置文件，让配置文件里面的内容生效；

--- spring-boot-02-config/src/test/java/com/atguigu/springboot/SpringBoot02ConfigApplicationTests.java
+++ spring-boot-02-config/src/test/java/com/atguigu/springboot/SpringBoot02ConfigApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -23,6 +24,15 @@ class SpringBoot02ConfigApplicationTests {
 	@Test
 	public void contextLoads() {
 		System.out.println(person);
+	}
+
+	@Autowired
+	ApplicationContext ioc;
+
+	@Test
+	public void testHelloService(){
+		boolean b = ioc.containsBean("helloService");
+		System.out.println(b);
 	}
 
 }
